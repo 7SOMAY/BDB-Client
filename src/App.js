f7387db8b74a459cbeab1681acfbe98ef78def71
7SOMAY <ssomay2002@gmail.com>
2023-08-07 16:46:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ export default function App() {
             dispatch({type: "CLEAR_MESSAGE"});
         }
     }, [error, message, dispatch]);
-
     useEffect(() => {
         dispatch(loadHome());
     }, [dispatch]);
