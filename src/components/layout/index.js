3d31e405e0105f4ee914f9982fd5b8822be816b3
7SOMAY <ssomay2002@gmail.com>
2023-08-08 11:51:05 +0530

    update

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,6 +1,6 @@
 export default function Index(props){
     return(
-        <div className={'flex items-center px-20 min-h-screen bg-gradient-to-r from-[#E3DFFD] to-[#ECF2FF] justify-center'}>
+        <div className={'flex items-center py-24 min-h-screen bg-gradient-to-r from-[#E3DFFD] to-[#ECF2FF] justify-center'}>
             {props.children}
         </div>
     )
